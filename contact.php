d33063ed9280630f0a7d8accbb3ade1d3cb5e412
Atheneyu <atheneyu2003@gmail.com>
2023-06-08 14:45:20 -0700

    changed buy button

--- contact.php
+++ contact.php
@@ -6,7 +6,18 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   $contactInfo = $_POST['contactInfo'];
   $message = $_POST['message'];
 
+  // Process the form data (e.g., send an email, store in a database, etc.)
 
-  $to = 'atheneyu2003@gmail.com';
+  // Example: Send an email
+  $to = 'your-email@example.com';
   $subject = 'New Contact Form Submission';
-  $emailBody = "Name: $name\nLanguage: $language\nPreferred Method: $preferredMethod\nContact Info: $contact\nMessage: $message";
+  $emailBody = "Name: $name\nLanguage: $language\nPreferred Method: $preferredMethod\nContact Info: $contactInfo\nMessage: $message";
+  $headers = "From: $to";
+
+  if (mail($to, $subject, $emailBody, $headers)) {
+    echo 'Thank you for your message. We will get back to you soon!';
+  } else {
+    echo 'Sorry, there was an error sending your message.';
+  }
+}
+?>
